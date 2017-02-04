923f42a408a2ae6881dcb14bd07ea654f27f3994
cesarsk <luca.cesarano1@gmail.com>
2017-02-04 19:43:40 +0100

    Aggiunta Activity

--- app/src/main/java/com/example/cesarsk/say_it/MainActivity.java
+++ app/src/main/java/com/example/cesarsk/say_it/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cesarsk.say_it;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
@@ -15,13 +16,17 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        final BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.selectTabAtPosition(1);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelected(@IdRes int tabId) {
-                if (tabId == R.id.tab_favorites) {
+                if (tabId == R.id.tab_settings) {
                     // The tab with id R.id.tab_favorites was selected,
                     // change your content accordingly.
+                    Class c = SettingsActivity.class;
+                    startActivity(new Intent(bottomBar.getContext(), c));
+
                 }
             }
         });
