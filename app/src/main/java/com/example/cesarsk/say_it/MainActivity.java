dfd82dd9b11c1d25bf1a7752e0480a6041982bdc
cesarsk <luca.cesarano1@gmail.com>
2017-02-05 17:06:49 +0100

    - Fix Bottom Bar - Preparing Bottom bars' Listener's code

--- app/src/main/java/com/example/cesarsk/say_it/MainActivity.java
+++ app/src/main/java/com/example/cesarsk/say_it/MainActivity.java
@@ -1,31 +1,69 @@
 package com.example.cesarsk.say_it;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.app.Fragment;
 import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
+import com.ncapdevi.fragnav.FragNavController;
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabReselectListener;
 import com.roughike.bottombar.OnTabSelectListener;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends FragmentActivity {
+
+    private FragNavController fragNavController;
+
+    private final int TAB_FIRST = FragNavController.TAB1;
+    private final int TAB_SECOND = FragNavController.TAB2;
+    private final int TAB_THIRD = FragNavController.TAB3;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //FragNav - list of fragments
+        List<Fragment> fragments = new ArrayList<>(3);
+
+        //add fragments to list
+  //      fragments.add(FirstFragment.newInstance(0));
+  //      fragments.add(SecondFragment.newInstance(0));
+  //      fragments.add(ThirdFragment.newInstance(0));
+
+        //link fragments to container
+  //      fragNavController = new FragNavController(getSupportFragmentManager(),R.id.bottomBar,fragments);
+        //End of FragNav
+
         final BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+
         bottomBar.selectTabAtPosition(1);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelected(@IdRes int tabId) {
-                if (tabId == R.id.tab_settings) {
+                if (tabId == R.id.tab_favorites) {
                     // The tab with id R.id.tab_favorites was selected,
                     // change your content accordingly.
-                    Class c = SettingsActivity.class;
-                    startActivity(new Intent(bottomBar.getContext(), c));
+                }
+
+                else if(tabId == R.id.tab_search) {
+
+                }
+
+                else if(tabId == R.id.tab_home) {
+
+                }
+
+                else if(tabId == R.id.tab_history) {
+
+                }
+
+                else if(tabId == R.id.tab_settings) {
 
                 }
             }
@@ -35,8 +73,24 @@ public class MainActivity extends Activity {
             @Override
             public void onTabReSelected(@IdRes int tabId) {
                 if (tabId == R.id.tab_favorites) {
-                    // The tab with id R.id.tab_favorites was reselected,
+                    // The tab with id R.id.tab_favorites was selected,
                     // change your content accordingly.
+                }
+
+                else if(tabId == R.id.tab_search) {
+
+                }
+
+                else if(tabId == R.id.tab_home) {
+
+                }
+
+                else if(tabId == R.id.tab_history) {
+
+                }
+
+                else if(tabId == R.id.tab_settings) {
+
                 }
             }
         });
