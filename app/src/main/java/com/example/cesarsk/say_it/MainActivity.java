9cb3527c7849c262054086b0e9df612d4da1b36c
cesarsk <luca.cesarano1@gmail.com>
2017-02-06 16:00:07 +0100

    Starting to work on Text To Speech feature

--- app/src/main/java/com/example/cesarsk/say_it/MainActivity.java
+++ app/src/main/java/com/example/cesarsk/say_it/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.cesarsk.say_it;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Handler;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.roughike.bottombar.BottomBar;
@@ -18,7 +21,10 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
+
+import static android.speech.tts.TextToSpeech.QUEUE_ADD;
 
 
 public class MainActivity extends FragmentActivity {
@@ -30,6 +36,10 @@ public class MainActivity extends FragmentActivity {
     private final int SEARCH_FRAGMENT_INDEX = 3;
     private final int SETTINGS_FRAGMENT_INDEX = 4;
 
+    //Definizione variabile TTS
+    private TextToSpeech tts;
+
+    //Definizione variabile WordList
     private ArrayList<String> WordList;
 
     @Override
@@ -81,5 +91,41 @@ public class MainActivity extends FragmentActivity {
         }
 
         Toast.makeText(this, "Caricate " + WordList.size() + " parole.", Toast.LENGTH_LONG).show();
+
+        //IMPOSTAZIONE TEXT TO SPEECH
+        tts= new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                // TODO Auto-generated method stub
+                if(status == TextToSpeech.SUCCESS){
+                    int result=tts.setLanguage(Locale.US);
+                    if(result==TextToSpeech.LANG_MISSING_DATA ||
+                            result==TextToSpeech.LANG_NOT_SUPPORTED){
+                        Log.e("error", "This Language is not supported");
+                    }
+                    else{
+
+                        //ConvertTextToSpeech();
+                        //tts.speak("Loaded "+WordList.size()+" words", QUEUE_ADD, null);
+                        tts.speak("Hello Luca, how are you? Let's start with a simple pronunciation of a word. Focus", QUEUE_ADD, null);
+
+                    }
+                }
+                else
+                    Log.e("error", "Initilization Failed!");
+            }
+        });
+
     }
 }
+/*
+    private void ConvertTextToSpeech() {
+        // TODO Auto-generated method stub
+        String text = et.getText().toString();
+        if(text==null||"".equals(text))
+        {
+            text = "Content not available";
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }else
+            tts.speak(text+"is saved", TextToSpeech.QUEUE_FLUSH, null);
+    } */
