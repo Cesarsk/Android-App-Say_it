206434985a5e906f31bdcd1f79739588ca15534f
Claffolo <claudioclaff@gmail.com>
2017-02-06 21:23:08 +0100

    Implementata la BinarySearch per la ricerca di una parola nella lista

--- app/src/main/java/com/example/cesarsk/say_it/MainActivity.java
+++ app/src/main/java/com/example/cesarsk/say_it/MainActivity.java
@@ -2,23 +2,23 @@ package com.example.cesarsk.say_it;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.os.Handler;
 import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.OnTabReselectListener;
 import com.roughike.bottombar.OnTabSelectListener;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Collections;
 
 
 public class MainActivity extends FragmentActivity {
@@ -80,6 +80,29 @@ public class MainActivity extends FragmentActivity {
             e.printStackTrace();
         }
 
+        Collections.sort(WordList);
+
         Toast.makeText(this, "Caricate " + WordList.size() + " parole.", Toast.LENGTH_LONG).show();
+    }
+
+    public void SearchWord(View view){
+        //FIXARE problema del non trovato!
+        EditText searchbox = (EditText) findViewById(R.id.SearchBox);
+        TextView result = (TextView) findViewById(R.id.Debug_Result_TextView);
+
+        String lookup = searchbox.getText().toString().toLowerCase();
+
+        if(!lookup.isEmpty()) {
+            int found_index = Collections.binarySearch(WordList, lookup);
+            if (found_index >= 0) {
+                result.setText("Trovata " + WordList.get(found_index) + " all'indice " + found_index);
+            }
+            else{
+                result.setText("Word not found!");
+            }
+        }
+        else{
+            result.setText("You have to write something!");
+        }
     }
 }
