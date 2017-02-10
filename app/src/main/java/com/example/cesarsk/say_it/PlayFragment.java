5942847aca6a9470a3b8dbba059d8018a1dbcb00
Shin94 <andreacroce54@gmail.com>
2017-02-10 15:15:43 +0100

    -Fix Permission: App not working!!

--- app/src/main/java/com/example/cesarsk/say_it/PlayFragment.java
+++ app/src/main/java/com/example/cesarsk/say_it/PlayFragment.java
@@ -1,20 +1,36 @@
 package com.example.cesarsk.say_it;
 
 import android.content.Context;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class PlayFragment extends Fragment {
 
     public PlayFragment() {
         // Required empty public constructor
     }
+
+    private static final String AUDIO_RECORDER_FILE_EXT_3GP = ".3gp";
+    private static final String AUDIO_RECORDER_FILE_EXT_MP4 = ".mp4";
+    private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
+    private MediaRecorder recorder = null;
+    private int currentFormat = 0;
+    private int output_formats[] = {MediaRecorder.OutputFormat.MPEG_4, MediaRecorder.OutputFormat.THREE_GPP};
+    private String file_exts[] = {AUDIO_RECORDER_FILE_EXT_MP4, AUDIO_RECORDER_FILE_EXT_3GP};
 
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
@@ -23,22 +39,78 @@ public class PlayFragment extends Fragment {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_play,
                 container, false);
+        ImageButton rec_button = (ImageButton) view.findViewById(R.id.rec_button);
 
-        view.setOnTouchListener(new View.OnTouchListener() {
-
+        rec_button.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch(event.getAction()){
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        Log.i("Say it!", "Start Recording");
+                        startRecording();
+                        break;
                     case MotionEvent.ACTION_UP:
-                    {
-                        Toast.makeText(container.getContext(), "HOLDING BUTTON", Toast.LENGTH_SHORT).show();
-                        return true; //indicate we're done listening to this touch listener
-                    }
+                        Log.i("Say it!", "Stop Recording");
+                        stopRecording();
+                        break;
                 }
                 return false;
             }
         });
-
         return view;
+    }
+
+    private String getFilename() {
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+        File file = new File(filepath, AUDIO_RECORDER_FOLDER);
+
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        return (file.getAbsolutePath() + "/" + System.currentTimeMillis() + file_exts[currentFormat]);
+    }
+
+    private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(output_formats[currentFormat]);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFile(getFilename());
+        recorder.setOnErrorListener(errorListener);
+        recorder.setOnInfoListener(infoListener);
+
+        try {
+            recorder.prepare();
+            recorder.start();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private MediaRecorder.OnErrorListener errorListener = new MediaRecorder.OnErrorListener() {
+        @Override
+        public void onError(MediaRecorder mr, int what, int extra) {
+            Log.i("Say it!","Error: " + what + ", " + extra);
+        }
+    };
+
+    private MediaRecorder.OnInfoListener infoListener = new MediaRecorder.OnInfoListener() {
+        @Override
+        public void onInfo(MediaRecorder mr, int what, int extra) {
+           Log.i("Say it!","Warning: " + what + ", " + extra);
+        }
+    };
+
+    private void stopRecording() {
+        if (null != recorder) {
+            recorder.stop();
+            recorder.reset();
+            recorder.release();
+
+            recorder = null;
+        }
     }
 }
