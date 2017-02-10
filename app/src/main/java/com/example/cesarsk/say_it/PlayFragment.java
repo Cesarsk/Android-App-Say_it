0e103af9057a6faa2a1291ffea62779d7aa09e85
Shin94 <andreacroce54@gmail.com>
2017-02-10 15:15:43 +0100

    App can now record: - Recorded button created; - Permissions methods added; - Minor cleanup.

--- app/src/main/java/com/example/cesarsk/say_it/PlayFragment.java
+++ app/src/main/java/com/example/cesarsk/say_it/PlayFragment.java
@@ -1,20 +1,43 @@
 package com.example.cesarsk.say_it;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class PlayFragment extends Fragment {
 
     public PlayFragment() {
         // Required empty public constructor
     }
+
+    private static final String AUDIO_RECORDER_FILE_EXT_AAC = ".aac.nomedia";
+    private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
+    private MediaRecorder recorder = null;
+    private int currentFormat = 0;
+    private int output_formats[] = {MediaRecorder.OutputFormat.DEFAULT};
+    private String file_exts[] = {AUDIO_RECORDER_FILE_EXT_AAC};
+    public static final int RequestPermissionCode = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
@@ -23,17 +46,30 @@ public class PlayFragment extends Fragment {
         // Inflate the layout for this fragment
         final View view = inflater.inflate(R.layout.fragment_play,
                 container, false);
+        ImageButton rec_button = (ImageButton) view.findViewById(R.id.rec_button);
 
-        view.setOnTouchListener(new View.OnTouchListener() {
-
+        rec_button.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch(event.getAction()){
-                    case MotionEvent.ACTION_UP:
-                    {
-                        Toast.makeText(container.getContext(), "HOLDING BUTTON", Toast.LENGTH_SHORT).show();
-                        return true; //indicate we're done listening to this touch listener
+            public boolean onTouch(View view, MotionEvent event) {
+
+                if (checkPermission())
+                {
+                    switch (event.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+                            Log.i("Say it!", "Start Recording");
+                            startRecording();
+                            break;
+
+                        case MotionEvent.ACTION_UP:
+                            Log.i("Say it!", "Stop Recording");
+                            stopRecording();
+                            break;
                     }
+                }
+
+                else
+                {
+                    requestPermission();
                 }
                 return false;
             }
@@ -41,4 +77,95 @@ public class PlayFragment extends Fragment {
 
         return view;
     }
+
+    private String getFilename() {
+        String filepath = Environment.getExternalStorageDirectory().getPath();
+        File file = new File(filepath, AUDIO_RECORDER_FOLDER);
+
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        return (file.getAbsolutePath() + "/" + System.currentTimeMillis() + file_exts[currentFormat]);
+    }
+
+    private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(output_formats[currentFormat]);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.HE_AAC);
+        recorder.setOutputFile(getFilename());
+        recorder.setOnErrorListener(errorListener);
+        recorder.setOnInfoListener(infoListener);
+
+        try {
+            recorder.prepare();
+            recorder.start();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private MediaRecorder.OnErrorListener errorListener = new MediaRecorder.OnErrorListener() {
+        @Override
+        public void onError(MediaRecorder mr, int what, int extra) {
+            Log.i("Say it!","Error: " + what + ", " + extra);
+        }
+    };
+
+    private MediaRecorder.OnInfoListener infoListener = new MediaRecorder.OnInfoListener() {
+        @Override
+        public void onInfo(MediaRecorder mr, int what, int extra) {
+           Log.i("Say it!","Warning: " + what + ", " + extra);
+        }
+    };
+
+    private void stopRecording() {
+        if (null != recorder) {
+            recorder.stop();
+            recorder.reset();
+            recorder.release();
+
+            recorder = null;
+        }
+    }
+
+    //FUNZIONI PER RICHIESTA PERMESSI
+    public boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getActivity(),
+                WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getActivity(),
+                RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED &&
+                result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(getActivity(), new
+                String[]{WRITE_EXTERNAL_STORAGE, RECORD_AUDIO}, RequestPermissionCode);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case RequestPermissionCode:
+                if (grantResults.length> 0) {
+                    boolean StoragePermission = grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED;
+                    boolean RecordPermission = grantResults[1] ==
+                            PackageManager.PERMISSION_GRANTED;
+
+                    if (StoragePermission && RecordPermission) {
+                        Toast.makeText(getActivity(), "Permission Granted", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getActivity(),"Permission Denied",Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+        }
+    }
+
 }
