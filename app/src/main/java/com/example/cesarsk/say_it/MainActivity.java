3947f42a1cbb427ce624437c8a82c4adea35268b
cesarsk <luca.cesarano1@gmail.com>
2017-03-01 18:01:05 +0100

    Code cleaning

--- app/src/main/java/com/example/cesarsk/say_it/MainActivity.java
+++ app/src/main/java/com/example/cesarsk/say_it/MainActivity.java
@@ -287,48 +287,7 @@ public class MainActivity extends AppCompatActivity {
         slide.setDuration(1000);
         getWindow().setExitTransition(slide);
     }
-
-    /**
-     * Showing google speech input dialog
-     * */
-    private void promptSpeechInput() {
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
-                getString(R.string.speech_prompt));
-        try {
-            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
-        } catch (ActivityNotFoundException a) {
-            Toast.makeText(getApplicationContext(),
-                    getString(R.string.speech_not_supported),
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    /**
-     * Receiving speech input
-     * */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case REQ_CODE_SPEECH_INPUT: {
-                if (resultCode == RESULT_OK && null != data) {
-
-                    ArrayList<String> result = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    editText.setText(result.get(0));
-                    //TODO AVVIARE ATTIVITA' CON TESTO RESULT.GET(0)
-                }
-                break;
-            }
-
-        }
-    }
-
+    
     boolean doubleBackToExitPressedOnce = false;
 
     @Override
