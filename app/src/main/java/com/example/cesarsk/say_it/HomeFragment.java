e255143bc6dd029d63327f6dbce359f280387ef2
Claffolo <claudioclaff@gmail.com>
2017-03-03 16:40:22 +0100

    Updated Home Fragment Layout

--- app/src/main/java/com/example/cesarsk/say_it/HomeFragment.java
+++ app/src/main/java/com/example/cesarsk/say_it/HomeFragment.java
@@ -24,7 +24,6 @@ public class HomeFragment extends Fragment {
     boolean anim_direction2= false;
     boolean anim_direction3 = false;
     boolean anim_direction4 = false;
-    boolean anim_direction5 = false;
     boolean anim_direction6 = false;
     boolean anim_direction7 = false;
     boolean anim_direction8 = false;
@@ -60,7 +59,6 @@ public class HomeFragment extends Fragment {
         final TextView second_wotd = (TextView)view.findViewById(R.id.second_wotd);
         final TextView third_wotd = (TextView)view.findViewById(R.id.third_wotd);
         final TextView fourth_wotd = (TextView)view.findViewById(R.id.fourth_wotd);
-        final TextView fifth_wotd = (TextView)view.findViewById(R.id.fifth_wotd);
         final TextView sixth_wotd = (TextView)view.findViewById(R.id.sixth_wotd);
         final TextView seventh_wotd = (TextView)view.findViewById(R.id.seventh_wotd);
         final TextView eighth_wotd = (TextView)view.findViewById(R.id.eighth_wotd);
@@ -74,8 +72,6 @@ public class HomeFragment extends Fragment {
         third_wotd.setPaintFlags(third_wotd.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         fourth_wotd.setText(Utility.getRandomWord(getActivity()));
         fourth_wotd.setPaintFlags(fourth_wotd.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-        fifth_wotd.setText(Utility.getRandomWord(getActivity()));
-        fifth_wotd.setPaintFlags(fifth_wotd.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         sixth_wotd.setText(Utility.getRandomWord(getActivity()));
         sixth_wotd.setPaintFlags(sixth_wotd.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         seventh_wotd.setText(Utility.getRandomWord(getActivity()));
@@ -105,11 +101,6 @@ public class HomeFragment extends Fragment {
         anim4.setRepeatCount(Animation.INFINITE);
         anim4.setRepeatMode(Animation.REVERSE);
 
-        AlphaAnimation anim5 = new AlphaAnimation(0.0f, 1.0f);
-        anim5.setDuration(5000 + (int)(Math.random() * 8000));
-        anim5.setRepeatCount(Animation.INFINITE);
-        anim5.setRepeatMode(Animation.REVERSE);
-
         AlphaAnimation anim6 = new AlphaAnimation(0.0f, 1.0f);
         anim6.setDuration(5000 + (int)(Math.random() * 8000));
         anim6.setRepeatCount(Animation.INFINITE);
@@ -134,7 +125,6 @@ public class HomeFragment extends Fragment {
         second_wotd.startAnimation(anim2);
         third_wotd.startAnimation(anim3);
         fourth_wotd.startAnimation(anim4);
-        fifth_wotd.startAnimation(anim5);
         sixth_wotd.startAnimation(anim6);
         seventh_wotd.startAnimation(anim7);
         eighth_wotd.startAnimation(anim8);
@@ -207,23 +197,6 @@ public class HomeFragment extends Fragment {
             public void onAnimationRepeat(Animation animation) {
                 if(anim_direction4) fourth_wotd.setText(Utility.getRandomWord(getActivity()));
                 anim_direction4 = !anim_direction4;
-            }
-        });
-        anim5.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-                if(anim_direction5) fifth_wotd.setText(Utility.getRandomWord(getActivity()));
-                anim_direction5 = !anim_direction5;
             }
         });
         anim6.setAnimationListener(new Animation.AnimationListener() {
