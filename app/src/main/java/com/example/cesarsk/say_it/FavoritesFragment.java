b6edcd14bcd58046d9db24e19db4857eee8595c5
Claffolo <claudioclaff@gmail.com>
2017-03-20 21:28:31 +0100

    Implemented Swipe to Delete in the Favorites and History Lists.

--- app/src/main/java/com/example/cesarsk/say_it/FavoritesFragment.java
+++ app/src/main/java/com/example/cesarsk/say_it/FavoritesFragment.java
@@ -3,7 +3,12 @@ package com.example.cesarsk.say_it;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.util.Pair;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
@@ -14,6 +19,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -25,6 +31,7 @@ import com.google.gson.Gson;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 
 import static android.speech.tts.TextToSpeech.QUEUE_FLUSH;
 import static com.example.cesarsk.say_it.MainActivity.american_speaker_google;
@@ -75,12 +82,21 @@ public class FavoritesFragment extends Fragment {
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         DefaultItemAnimator defaultItemAnimator = new DefaultItemAnimator();
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getActivity(),linearLayoutManager.getOrientation());
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getActivity(), linearLayoutManager.getOrientation());
 
         final FavoritesAdapter adapter = new FavoritesAdapter(DeserializedFavs);
         recyclerView.setAdapter(adapter);
 
         ItemTouchHelper touchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            Drawable background;
+            boolean initiated = false;
+
+            private void init(){
+                background = new ColorDrawable(ContextCompat.getColor(getActivity(), R.color.Red500));
+                initiated = true;
+            }
+
             @Override
             public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
                 return false;
@@ -88,9 +104,38 @@ public class FavoritesFragment extends Fragment {
 
             @Override
             public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-                Utility.removeFavs(getActivity(), adapter.getFavorites().get(viewHolder.getAdapterPosition()));
-                adapter.setFavorites(loadDeserializedFavs(getActivity()));
-                adapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+                adapter.addToPendingRemoval(viewHolder.getAdapterPosition());
+            }
+
+            @Override
+            public int getSwipeDirs(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                int position = viewHolder.getAdapterPosition();
+                if (adapter.isPendingRemoval(position)) {
+                    return 0;
+                }
+                return super.getSwipeDirs(recyclerView, viewHolder);
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                View itemView = viewHolder.itemView;
+
+                // not sure why, but this method get's called for viewholder that are already swiped away
+                if (viewHolder.getAdapterPosition() == -1) {
+                    // not interested in those
+                    return;
+                }
+
+                if (!initiated) {
+                    init();
+                }
+
+                // draw red background
+                background.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+                background.draw(c);
+
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
             }
         });
         recyclerView.addItemDecoration(dividerItemDecoration);
@@ -103,18 +148,17 @@ public class FavoritesFragment extends Fragment {
 
     private class FavoritesAdapter extends RecyclerView.Adapter<FavoritesAdapter.ViewHolder> {
 
-        public ArrayList<Pair<String, String>> getFavorites() {
-            return favorites;
-        }
-
-        public void setFavorites(ArrayList<Pair<String, String>> favorites) {
-            this.favorites = favorites;
-        }
+        public static final long UNDO_TIMEOUT = 3000; //Timeout prima che l'elemento venga cancellato definitivamente
 
         private ArrayList<Pair<String, String>> favorites;
+        private ArrayList<Pair<String, String>> pendingFavorites;
+        private Handler handler = new Handler(); //Handler per gestire i Runnable per permettere l'UNDO con il Delay
+        HashMap<Pair<String, String>, Runnable> pendingRunnables = new HashMap<>(); //HashMap che associa ad ogni elemento della lista un Runnable che aspetterà
+        //3 secondi prima di cancellare l'elemento dalla lista.
 
         FavoritesAdapter(ArrayList<Pair<String, String>> favorites_list) {
             favorites = favorites_list;
+            pendingFavorites = new ArrayList<>();
         }
 
         class ViewHolder extends RecyclerView.ViewHolder {
@@ -123,6 +167,7 @@ public class FavoritesFragment extends Fragment {
             TextView IPATextView;
             ImageButton QuickPlayBtn;
             ImageButton AddtoFavsBtn;
+            TextView UndoButton;
 
             ViewHolder(View itemView) {
                 super(itemView);
@@ -130,6 +175,7 @@ public class FavoritesFragment extends Fragment {
                 IPATextView = (TextView) itemView.findViewById(R.id.list_item_second_line);
                 QuickPlayBtn = (ImageButton) itemView.findViewById(R.id.list_item_quickplay);
                 AddtoFavsBtn = (ImageButton) itemView.findViewById(R.id.list_item_addToFavs);
+                UndoButton = (TextView) itemView.findViewById(R.id.undo_btn);
             }
         }
 
@@ -146,43 +192,108 @@ public class FavoritesFragment extends Fragment {
         @Override
         public void onBindViewHolder(final FavoritesAdapter.ViewHolder holder, int position) {
 
-            holder.wordTextView.setText(favorites.get(position).first);
-            holder.IPATextView.setText(favorites.get(position).second);
+            final Pair<String, String> current_item = favorites.get(position);
 
-            holder.QuickPlayBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //Cliccando su Play Button nella search result tab riproduce play.
-                    american_speaker_google.speak(holder.wordTextView.getText(), QUEUE_FLUSH, null, null);
-                }
-            });
-
-            final boolean favorite_flag = Utility.checkFavs(getActivity(), favorites.get(position).first);
-            if (favorite_flag)
-                holder.AddtoFavsBtn.setColorFilter(getResources().getColor(R.color.RudolphsNose));
-
-            holder.AddtoFavsBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (!favorite_flag) {
-                        Utility.addFavs(getActivity(), new Pair<>(holder.wordTextView.getText().toString(), holder.IPATextView.getText().toString()));
-                        Toast.makeText(getActivity(), "Added to Favorites", Toast.LENGTH_SHORT).show();
+            if (pendingFavorites.contains(current_item)) {
+                holder.itemView.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.Red500));
+                holder.wordTextView.setVisibility(View.GONE);
+                holder.IPATextView.setVisibility(View.GONE);
+                holder.QuickPlayBtn.setVisibility(View.GONE);
+                holder.AddtoFavsBtn.setVisibility(View.GONE);
+                holder.UndoButton.setVisibility(View.VISIBLE);
+                holder.UndoButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Runnable pendingRemovalRunnable = pendingRunnables.get(current_item);
+                        pendingRunnables.remove(current_item);
+                        if (pendingRemovalRunnable != null) {
+                            handler.removeCallbacks(pendingRemovalRunnable);
+                        }
+                        pendingFavorites.remove(current_item);
+                        // this will rebind the row in "normal" state
+                        notifyItemChanged(favorites.indexOf(current_item));
                     }
+                });
+            } else {
+                holder.wordTextView.setText(favorites.get(position).first);
+                holder.IPATextView.setText(favorites.get(position).second);
 
-                    if (favorite_flag) {
-                        Utility.removeFavs(v.getContext(), favorites.get(holder.getAdapterPosition()));
-                        favorites = loadDeserializedFavs(getActivity());
-                        notifyItemRemoved(holder.getAdapterPosition());
-                        Toast.makeText(getActivity(), "Removed from Favorites", Toast.LENGTH_SHORT).show();
+                holder.QuickPlayBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Cliccando su Play Button nella search result tab riproduce play.
+                        american_speaker_google.speak(holder.wordTextView.getText(), QUEUE_FLUSH, null, null);
                     }
-                }
-            });
+                });
+
+                final boolean favorite_flag = Utility.checkFavs(getActivity(), favorites.get(position).first);
+                if (favorite_flag)
+                    holder.AddtoFavsBtn.setColorFilter(ContextCompat.getColor(getActivity(), R.color.RudolphsNose));
+
+                holder.AddtoFavsBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (!favorite_flag) {
+                            add(holder);
+                        }
+
+                        if (favorite_flag) {
+                            remove(holder.getAdapterPosition());
+                        }
+                    }
+                });
+            }
 
         }
 
         @Override
         public int getItemCount() {
             return favorites.size();
+        }
+
+        public void addToPendingRemoval(int position){
+            final Pair<String, String> item = favorites.get(position);
+
+            if (!pendingFavorites.contains(item)) {
+                pendingFavorites.add(item);
+                //Si notifica l'adapter in modo tale da ridisegnare la view
+                notifyItemChanged(position);
+
+                //Creazione del Runnable per l'attesa di 3 secondi
+                Runnable pendingRemovalRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        remove(favorites.indexOf(item));
+                    }
+                };
+                handler.postDelayed(pendingRemovalRunnable, UNDO_TIMEOUT);
+                pendingRunnables.put(item, pendingRemovalRunnable);
+            }
+        }
+
+
+        public boolean isPendingRemoval(int position) {
+            return pendingFavorites.contains(favorites.get(position));
+        }
+
+        public void remove(int pos) {
+            Utility.removeFavs(getActivity(), favorites.get(pos));
+            favorites = loadDeserializedFavs(getActivity());
+            notifyItemRemoved(pos);
+            Toast.makeText(getActivity(), "Removed from Favorites", Toast.LENGTH_SHORT).show();
+        }
+
+        public void add(FavoritesAdapter.ViewHolder viewHolder) {
+            Utility.addFavs(getActivity(), new Pair<>(viewHolder.wordTextView.getText().toString(), viewHolder.IPATextView.getText().toString()));
+            Toast.makeText(getActivity(), "Added to Favorites", Toast.LENGTH_SHORT).show();
+        }
+
+        public ArrayList<Pair<String, String>> getFavorites() {
+            return favorites;
+        }
+
+        public void setFavorites(ArrayList<Pair<String, String>> favorites) {
+            this.favorites = favorites;
         }
     }
 }
--- app/src/main/java/com/example/cesarsk/say_it/HistoryFragment.java
+++ app/src/main/java/com/example/cesarsk/say_it/HistoryFragment.java
@@ -3,8 +3,13 @@ package com.example.cesarsk.say_it;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.util.Pair;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
@@ -25,6 +30,7 @@ import com.google.gson.Gson;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 
 import static android.speech.tts.TextToSpeech.QUEUE_FLUSH;
 import static com.example.cesarsk.say_it.MainActivity.american_speaker_google;
@@ -53,7 +59,7 @@ public class HistoryFragment extends Fragment {
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         DefaultItemAnimator defaultItemAnimator = new DefaultItemAnimator();
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getActivity(),linearLayoutManager.getOrientation());
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getActivity(), linearLayoutManager.getOrientation());
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setItemAnimator(defaultItemAnimator);
         recyclerView.addItemDecoration(dividerItemDecoration);
@@ -61,6 +67,15 @@ public class HistoryFragment extends Fragment {
         recyclerView.setAdapter(adapter);
 
         ItemTouchHelper touchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+            Drawable background;
+            boolean initiated = false;
+
+            private void init(){
+                background = new ColorDrawable(ContextCompat.getColor(getActivity(), R.color.Red500));
+                initiated = true;
+            }
+
             @Override
             public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
                 return false;
@@ -68,9 +83,39 @@ public class HistoryFragment extends Fragment {
 
             @Override
             public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-                Utility.removeHist(getActivity(), adapter.getHistory().get(viewHolder.getAdapterPosition()));
-                adapter.setHistory(loadDeserializedHistory(getActivity()));
-                adapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+                adapter.addToPendingRemoval(viewHolder.getAdapterPosition());
+            }
+
+            //TODO implementare swipe to delete con UNDO
+            @Override
+            public int getSwipeDirs(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+                int position = viewHolder.getAdapterPosition();
+                if (adapter.isPendingRemoval(position)) {
+                    return 0;
+                }
+                return super.getSwipeDirs(recyclerView, viewHolder);
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                View itemView = viewHolder.itemView;
+
+                // not sure why, but this method get's called for viewholder that are already swiped away
+                if (viewHolder.getAdapterPosition() == -1) {
+                    // not interested in those
+                    return;
+                }
+
+                if (!initiated) {
+                    init();
+                }
+
+                // draw red background
+                background.setBounds(itemView.getRight() + (int) dX, itemView.getTop(), itemView.getRight(), itemView.getBottom());
+                background.draw(c);
+
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
             }
         });
 
@@ -81,6 +126,8 @@ public class HistoryFragment extends Fragment {
 
     private class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.ViewHolder> {
 
+        public static final long UNDO_TIMEOUT = 3000; //Timeout prima che l'elemento venga cancellato definitivamente
+
         public ArrayList<SayItPair> getHistory() {
             return history;
         }
@@ -90,9 +137,15 @@ public class HistoryFragment extends Fragment {
         }
 
         private ArrayList<SayItPair> history;
+        private ArrayList<SayItPair> pendingElements;
+        private Handler handler = new Handler(); //Handler per gestire i Runnable per permettere l'UNDO con il Delay
+        HashMap<Pair<String, String>, Runnable> pendingRunnables = new HashMap<>(); //HashMap che associa ad ogni elemento della lista un Runnable che aspetterà
+        //3 secondi prima di cancellare l'elemento dalla lista.
 
         public HistoryAdapter(ArrayList<SayItPair> history_list) {
+
             history = history_list;
+            pendingElements = new ArrayList<>();
         }
 
         class ViewHolder extends RecyclerView.ViewHolder {
@@ -102,19 +155,16 @@ public class HistoryFragment extends Fragment {
             ImageButton QuickPlayBtn;
             ImageButton AddtoFavsBtn;
             ImageButton DeleteFromHistoryBtn;
+            TextView UndoButton;
 
             ViewHolder(View itemView) {
                 super(itemView);
                 wordTextView = (TextView) itemView.findViewById(R.id.list_item_first_line);
-                ;
                 IPATextView = (TextView) itemView.findViewById(R.id.list_item_second_line);
-                ;
                 QuickPlayBtn = (ImageButton) itemView.findViewById(R.id.list_item_quickplay);
-                ;
                 AddtoFavsBtn = (ImageButton) itemView.findViewById(R.id.list_item_addToFavs);
-                ;
                 DeleteFromHistoryBtn = (ImageButton) itemView.findViewById(R.id.list_item_removeFromHistory);
-                ;
+                UndoButton = (TextView) itemView.findViewById(R.id.undo_btn);
             }
         }
 
@@ -131,48 +181,70 @@ public class HistoryFragment extends Fragment {
         @Override
         public void onBindViewHolder(final HistoryAdapter.ViewHolder holder, int position) {
 
-            holder.wordTextView.setText(history.get(position).first);
-            holder.IPATextView.setText(history.get(position).second);
+            final SayItPair current_item = history.get(position);
 
-            holder.QuickPlayBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    //Cliccando su Play Button nella search result tab riproduce play.
-                    american_speaker_google.speak(holder.wordTextView.getText(), QUEUE_FLUSH, null, null);
-                }
-            });
-
-            final boolean favorite_flag = Utility.checkFavs(getActivity(), history.get(position).first);
-            if (favorite_flag)
-                holder.AddtoFavsBtn.setColorFilter(getResources().getColor(R.color.RudolphsNose));
-
-            holder.AddtoFavsBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (!favorite_flag) {
-                        Utility.addFavs(getActivity(), new Pair<>(holder.wordTextView.getText().toString(), holder.IPATextView.getText().toString()));
-                        Toast.makeText(getActivity(), "Added to Favorites", Toast.LENGTH_SHORT).show();
-                        holder.AddtoFavsBtn.setColorFilter(getResources().getColor(R.color.RudolphsNose));
+            if (pendingElements.contains(current_item)) {
+                holder.itemView.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.Red500));
+                holder.wordTextView.setVisibility(View.GONE);
+                holder.IPATextView.setVisibility(View.GONE);
+                holder.QuickPlayBtn.setVisibility(View.GONE);
+                holder.AddtoFavsBtn.setVisibility(View.GONE);
+                holder.DeleteFromHistoryBtn.setVisibility(View.GONE);
+                holder.UndoButton.setVisibility(View.VISIBLE);
+                holder.UndoButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Runnable pendingRemovalRunnable = pendingRunnables.get(current_item);
+                        pendingRunnables.remove(current_item);
+                        if (pendingRemovalRunnable != null) {
+                            handler.removeCallbacks(pendingRemovalRunnable);
+                        }
+                        pendingElements.remove(current_item);
+                        // this will rebind the row in "normal" state
+                        notifyItemChanged(history.indexOf(current_item));
                     }
+                });
+            } else {
+                holder.wordTextView.setText(history.get(position).first);
+                holder.IPATextView.setText(history.get(position).second);
 
-                    if (favorite_flag) {
-                        Utility.removeFavs(v.getContext(), history.get(holder.getAdapterPosition()));
-                        Toast.makeText(getActivity(), "Removed from Favorites", Toast.LENGTH_SHORT).show();
-                        holder.AddtoFavsBtn.setColorFilter(getResources().getColor(R.color.colorPrimaryDark));
+                holder.QuickPlayBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //Cliccando su Play Button nella search result tab riproduce play.
+                        american_speaker_google.speak(holder.wordTextView.getText(), QUEUE_FLUSH, null, null);
                     }
-                }
-            });
+                });
 
-            holder.DeleteFromHistoryBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    int pos = holder.getAdapterPosition();
-                    Utility.removeHist(view.getContext(), new SayItPair(history.get(pos).first, history.get(pos).second, history.get(pos).getAdding_time()));
-                    history = loadDeserializedHistory(getActivity());
-                    notifyItemRemoved(pos);
-                    Toast.makeText(getActivity(), "Removed from History", Toast.LENGTH_SHORT).show();
-                }
-            });
+                final boolean favorite_flag = Utility.checkFavs(getActivity(), history.get(position).first);
+                if (favorite_flag)
+                    holder.AddtoFavsBtn.setColorFilter(getResources().getColor(R.color.RudolphsNose));
+
+                holder.AddtoFavsBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (!favorite_flag) {
+                            Utility.addFavs(getActivity(), new Pair<>(holder.wordTextView.getText().toString(), holder.IPATextView.getText().toString()));
+                            Toast.makeText(getActivity(), "Added to Favorites", Toast.LENGTH_SHORT).show();
+                            holder.AddtoFavsBtn.setColorFilter(getResources().getColor(R.color.RudolphsNose));
+                        }
+
+                        if (favorite_flag) {
+                            Utility.removeFavs(v.getContext(), history.get(holder.getAdapterPosition()));
+                            Toast.makeText(getActivity(), "Removed from Favorites", Toast.LENGTH_SHORT).show();
+                            holder.AddtoFavsBtn.setColorFilter(getResources().getColor(R.color.colorPrimaryDark));
+                        }
+                    }
+                });
+
+                holder.DeleteFromHistoryBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int pos = holder.getAdapterPosition();
+                        remove(pos);
+                    }
+                });
+            }
 
         }
 
@@ -180,6 +252,39 @@ public class HistoryFragment extends Fragment {
         public int getItemCount() {
             return history.size();
         }
+
+        public void remove(int pos) {
+            Utility.removeHist(getActivity(), new SayItPair(history.get(pos).first, history.get(pos).second, history.get(pos).getAdding_time()));
+            history = loadDeserializedHistory(getActivity());
+            notifyItemRemoved(pos);
+            Toast.makeText(getActivity(), "Removed from History", Toast.LENGTH_SHORT).show();
+        }
+
+        public void addToPendingRemoval(int position){
+            final SayItPair item = history.get(position);
+
+            if (!pendingElements.contains(item)) {
+                pendingElements.add(item);
+                //Si notifica l'adapter in modo tale da ridisegnare la view
+                notifyItemChanged(position);
+
+                //Creazione del Runnable per l'attesa di 3 secondi
+                Runnable pendingRemovalRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        remove(history.indexOf(item));
+                    }
+                };
+                handler.postDelayed(pendingRemovalRunnable, UNDO_TIMEOUT);
+                pendingRunnables.put(item, pendingRemovalRunnable);
+            }
+        }
+
+
+        public boolean isPendingRemoval(int position) {
+            return pendingElements.contains(history.get(position));
+        }
+
     }
 
     public static ArrayList<SayItPair> loadDeserializedHistory(Context context) {
