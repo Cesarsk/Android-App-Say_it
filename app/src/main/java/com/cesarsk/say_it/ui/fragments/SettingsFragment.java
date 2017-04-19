0def56f8883afec04495b9a10570cd3500736cd3
Luca Cesarano <luca.cesarano1@gmail.com>
2017-04-19 18:48:53 +0200

    Updated acknowledgements

--- app/src/main/java/com/cesarsk/say_it/ui/fragments/SettingsFragment.java
+++ app/src/main/java/com/cesarsk/say_it/ui/fragments/SettingsFragment.java
@@ -11,6 +11,7 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.cesarsk.say_it.R;
@@ -19,7 +20,13 @@ import com.cesarsk.say_it.ui.MainActivity;
 import com.cesarsk.say_it.ui.PlayActivity;
 import com.cesarsk.say_it.utility.Utility;
 import com.cesarsk.say_it.utility.UtilitySharedPrefs;
+import com.cesarsk.say_it.utility.utility_aidl.IabHelper;
+import com.cesarsk.say_it.utility.utility_aidl.IabResult;
+import com.cesarsk.say_it.utility.utility_aidl.Inventory;
+import com.cesarsk.say_it.utility.utility_aidl.Purchase;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import static com.cesarsk.say_it.utility.Utility.rateUs;
@@ -34,7 +41,9 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
     private Callback mCallback;
     private static final String KEY_1 = "button_notification";
     private static final String KEY_2 = "open_source_licenses";
-
+    IabHelper mHelper;
+    IabHelper.QueryInventoryFinishedListener mQueryFinishedListener;
+    IabHelper.OnIabPurchaseFinishedListener mIabPurchaseFinishedListener;
 
     public interface Callback {
         public void onNestedPreferenceSelected(int key);
@@ -135,6 +144,65 @@ public class SettingsFragment extends PreferenceFragment implements Preference.O
                 return false;
             }
         });
+
+        mIabPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+            @Override
+            public void onIabPurchaseFinished(IabResult result, Purchase info) {
+                if (result.isFailure()) {
+                    Toast.makeText(getActivity(), "Purchase Failed! Perhaps have you already purchased the item?", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if (info.getSku().equals(PlayActivity.no_ads_in_app)) {
+                    UtilitySharedPrefs.loadAdsStatus(getActivity());
+                    UtilitySharedPrefs.savePrefs(getActivity(), true, MainActivity.NO_ADS_STATUS_KEY);
+                }
+            }
+        };
+
+        mQueryFinishedListener = new IabHelper.QueryInventoryFinishedListener() {
+            public void onQueryInventoryFinished(IabResult result, Inventory inventory)
+            {
+                if (result.isFailure()) {
+                    Toast.makeText(getActivity(), "Query Failed!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //Get in-app item price
+                String noAdsPrice = inventory.getSkuDetails(PlayActivity.no_ads_in_app).getPrice();
+
+                //Open Purchase Dialog
+                try {
+                    mHelper.launchPurchaseFlow(getActivity(), PlayActivity.no_ads_in_app, 64000, mIabPurchaseFinishedListener);
+                } catch (IabHelper.IabAsyncInProgressException e) {
+                    e.printStackTrace();
+                }
+                // update the UI
+            }
+        };
+
+        final Preference remove_ads = getPreferenceManager().findPreference("remove_ads");
+        if(MainActivity.NO_ADS)
+        {
+            remove_ads.setEnabled(false);
+            remove_ads.setSummary("Thank you for supporting us ❤");
+        }
+        else
+        {
+            remove_ads.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    //helper to remove ads
+                    List<String> additionalSkuList = new ArrayList<>();
+                    additionalSkuList.add(PlayActivity.no_ads_in_app);
+                    try {
+                        mHelper.queryInventoryAsync(true, additionalSkuList, mQueryFinishedListener);
+                    } catch (IabHelper.IabAsyncInProgressException e) {
+                        e.printStackTrace();
+                    }
+                    return false;
+                }
+            });
+        }
 
         final Preference reset_tutorial = getPreferenceManager().findPreference("reset_showcase");
         reset_tutorial.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
